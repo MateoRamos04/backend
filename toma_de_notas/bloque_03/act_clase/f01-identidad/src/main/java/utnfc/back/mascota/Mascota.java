a23d82d5a05a4560108f552ad7dffb4158f12079
Mateo Ramos <mateoiramos06@gmail.com>
2026-08-18 12:44:14 -0300

    Repo de cursada Backend de Aplicaciones

---
+++ toma_de_notas/bloque_03/act_clase/f01-identidad/src/main/java/utnfc/back/mascota/Mascota.java
@@ -0,0 +1,18 @@
+package main.java.utnfc.back.mascota;
+
+public class Mascota {
+    //constructor
+    public Mascota(String nombre) {
+        
+    }
+
+    // Propieadd nombre
+    // getters and Setters
+    public String getNombre() {
+        return null;
+    }
+
+    public void setNombre(String nombre) {
+        
+    }
+}
